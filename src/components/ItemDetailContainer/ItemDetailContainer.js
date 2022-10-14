e682844809c5df4b4a35dda73a259371cfb75d0e
Alessandro Orezzoli <orezzoliale@gmail.com>
2022-10-14 01:27:14 -0300

    arreglo de css

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -24,7 +24,7 @@ const ItemDetailContainer = () => {
     if (loading) {
 
         return (
-            <div>
+            <div className="loading">
                 < Waveform
                     size={40}
                     lineWeight={3.5}
